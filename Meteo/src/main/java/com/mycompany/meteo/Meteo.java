30d5fd12e6911f3361c7cf81401778dd96c69668
Sidy Diallo <diallosidymohamed99@gmail.com>
2024-04-24 02:17:26 +0200

    debut de l'interface et configuration de okHTTP pour la gestion des requettes http

---
+++ Meteo/src/main/java/com/mycompany/meteo/Meteo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.meteo;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class Meteo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new MeteoFrame("Meteo");
+    }
+}
---
+++ Meteo/src/main/java/com/mycompany/meteo/MeteoFrame.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.meteo;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class MeteoFrame extends JFrame {
+    public MeteoFrame(String title){
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(400,400);
+        setVisible(true);
+        
+        
+    }
+
+    
+}
